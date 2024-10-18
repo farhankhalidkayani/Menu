e0aa9a1fdbd2fe4e9c57dc15a71b77b4b5c4c461
Farhan Khalid <rj.farhan4232@gmail.com>
2024-10-18 12:46:06 +0500

    Added username of the user which posted the item

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -3,10 +3,9 @@ from . import views
 
 app_name = "food"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("item/", views.item, name="item"),
-    path("<int:item_id>/", views.detail, name="detail"),
-    path("add/", views.add_item, name="add_item"),
+    path("", views.index.as_view(), name="index"),
+    path("<int:pk>/", views.detail.as_view(), name="detail"),
+    path("add/", views.add_item.as_view(), name="add_item"),
     path("edit/<int:item_id>", views.edit_item, name="edit_item"),
     path("delete/<int:item_id>", views.delete_item, name="delete_item"),
 ]
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,36 +1,38 @@
 from multiprocessing import context
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 from .forms import ItemForm
 from .models import Item
 
 
 # Create your views here.
-def index(request):
-    item_list = Item.objects.all()
-    return render(request, "index.html", {"items": item_list})
 
 
-def item(request):
-    return HttpResponse("This is an item view")
+class index(ListView):
+    model = Item
+    template_name = "index.html"
+    context_object_name = "items"
 
 
-def detail(request, item_id):
-    item = Item.objects.get(pk=item_id)
-    return render(request, "detail.html", {"item": item})
+class detail(DetailView):
+    model = Item
+    template_name = "detail.html"
 
 
-def add_item(request):
-    if request.method == "POST":
-        form = ItemForm(request.POST)
+class add_item(CreateView):
+    model = Item
+    fields = ["item_name", "item_desc", "item_price", "item_img"]
+    template_name = "item-form.html"
+    success_url = reverse_lazy("food:index")
 
-        if form.is_valid():
-            form.save()
-            return redirect("food:index")
-    else:
-        form = ItemForm()
-    return render(request, "item-form.html", {"form": form})
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+        return super().form_valid(form)
 
 
 def edit_item(request, item_id):
